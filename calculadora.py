e09b004e603b97bbbc95dddf38dd1ebcfaedda8d
trademilima <202405013921@alunos.estacio.br>
2024-05-18 00:48:59 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+a=float(input('Digite o primeiro número:'))
+b=float(input('Digite o segundo número:'))
+
+print("Resultado da soma:"+str(a+b))
+print("Resultado da subtração:"+str(a-b))
+print("Resultado da multiplicação:"+str(a*b))
+print("Resultado da divisão:"+str(a/b))
